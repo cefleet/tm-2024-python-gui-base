d8e08330dbe217ca40a2090c484ea246a3c71e4c
Clint Fleetwood <clint.fleetwood@gmail.com>
2025-02-10 14:04:04 +0000

    added games with movements

--- game-hs.py
+++ game-hs.py
@@ -1,18 +1,26 @@
 import pyglet
 from pyglet.shapes import Rectangle, Circle
+from pyglet.window import key
 
+# windows config
 window_width = 800
 window_height = 600
 window = pyglet.window.Window(width=window_width, height=window_height, caption="My Game")
 
+#color constants
 GRAY = (169,169,169)
 GREEN = (34,139,34)
 WHITE = (255,255,255)
 
+#player config
 player_radius = 40
 player_x = window_width / 2
 player_y = 100
 player_speed = 150
+
+# This handles keyboard input
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
 
 player = Circle(player_x, player_y, player_radius, color=WHITE)
 
@@ -24,18 +32,19 @@ background = Rectangle(0,0,window_width, window_height, GREEN )
 path = Rectangle(path_x,0,path_width,window_height,GRAY)
 dir = 'right'
 def update(dt):
-
     global dir
 
-    if dir == 'right':
-        player.x = player.x + player_speed * dt
-    elif dir == 'left':
-        player.x = player.x - player_speed * dt
+    if keys[key.LEFT]:
+        player.x -= player_speed * dt
+    if keys[key.RIGHT]:
+        player.x += player_speed * dt
 
-    if player.x > path_width+path_x-player_radius and dir == 'right':
-        dir = 'left'
-    elif player.x < path_x+player_radius and dir =='left':
-        dir = 'right'
+    if player.x > path_width+path_x-player_radius:
+        player.x =  path_width+path_x-player_radius
+
+    if player.x < path_x+player_radius:
+        player.x = path_x + player_radius
+
 
 @window.event
 def on_draw():
--- game-ms.py
+++ game-ms.py
@@ -1,21 +1,30 @@
 import pyglet
 
 from pyglet.shapes import Rectangle, Circle
+from pyglet.window import Window, key
 
+# Window config
 window_width = 800
 window_height = 600
-window = pyglet.window.Window(height=window_height, width=window_width, caption="My Game")
+window = Window(height=window_height, width=window_width, caption="My Game")
 
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
+
+# Colors
 GRAY=(170,170,170)
 GREEN = (34,139,34)
 WHITE = (255,255,255)
 
+# path config
 path_width = 400
 path_x = (window_width-path_width)/2
 path = Rectangle(path_x,0,path_width,window_height,GRAY)
 
+# backgronud
 green_background = Rectangle(0,0, window_width, window_height, GREEN)
 
+# player config
 player_y = 100
 player_x = window_width / 2
 player_radius = 40
@@ -23,21 +32,22 @@ player_radius = 40
 player = Circle(player_x,player_y,player_radius, color=WHITE )
 player_speed = 150
 
-player_dir = 'left'
+#vplayer_dir = 'left'
 def update(dt):
-    global player_dir
-    if player_dir == 'right':
-        player.x = player.x + player_speed * dt
-    if player_dir == 'left':
-        player.x = player.x - player_speed * dt
 
-    if player_dir == 'right' and player.x > path_x + path_width - player_radius:
-        player_dir = 'left'
-    elif player_dir == 'left' and player.x < path_x + player_radius:
-        player_dir = 'right'
+    if keys[key.LEFT]:
+        player.x -= player_speed * dt
     
+    if keys[key.RIGHT]:
+        player.x += player_speed * dt
 
+    if player.x > path_width+path_x-player_radius:
+        player.x =  path_width+path_x-player_radius
 
+    if player.x < path_x+player_radius:
+        player.x = path_x + player_radius
+
+    
 @window.event
 def on_draw():
     window.clear()
